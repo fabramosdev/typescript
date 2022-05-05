8ae6755f79bb6f56544f35077bf733fb1db16b38
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2022-05-05 18:41:13 -0300

    array types adjust comment

--- typesArrays/src/script.ts
+++ typesArrays/src/script.ts
@@ -1,7 +1,9 @@
+// Exemplo: tipo[] = [aqui vem o array]
+
 let nomes: string[] = ['Fabiano', 'Camila', 'Helena', 'Bernardo', 'Miguel'];
 
 let idades: number[] = [44, 37, 6, 13, 15];
 
-// Outra forma: tipar com Array<tipo>
+// Outra forma: tipar com Array<tipo> = [aqui vem o array]
 
 let numeros: Array<number> = [1, 2, 3, 4, 5];
