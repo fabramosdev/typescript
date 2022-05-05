1b29629227b1ba151de762b9b1f9b1815b671065
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2022-05-05 18:33:13 -0300

    array types

---
+++ typesArrays/src/script.ts
@@ -0,0 +1,7 @@
+let nomes: string[] = ['Fabiano', 'Camila', 'Helena', 'Bernardo', 'Miguel'];
+
+let idades: number[] = [44, 37, 6, 13, 15];
+
+// Outra forma: tipar com Array<tipo>
+
+let numeros: Array<number> = [1, 2, 3, 4, 5];
