beeaf1e9b48f8afd3bbe81e5777e39568dbd4b73
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2022-05-05 23:40:24 -0300

    contextual type, objct type, optional props

---
+++ 008 - contextualTyping/public/script.js
@@ -0,0 +1,4 @@
+var names = ['Fabiano', 'Camila', 'Helena', 'Bernardo', 'Miguel'];
+names.forEach(function (name) {
+    console.log(name.toUpperCase());
+});
---
+++ 008 - contextualTyping/src/script.ts
@@ -0,0 +1,5 @@
+let names = ['Fabiano', 'Camila', 'Helena', 'Bernardo', 'Miguel'];
+
+names.forEach(function (name) {
+  console.log(name.toUpperCase());
+})
